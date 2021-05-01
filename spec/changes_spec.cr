811dd7eacbbc7764ae87c129716a9e76a896c3d2
Moisès Macià <mmacia@gmail.com>
2021-05-01 10:27:31 +0200

    changes endpoint

---
+++ spec/changes_spec.cr
@@ -0,0 +1,198 @@
+require "./spec_helper"
+
+describe Tmdb::Changes do
+  context "#movies" do
+    start_date = Time.utc(2013, 5, 20, 10, 0, 0)
+    end_date = Time.utc(2013, 5, 25, 10, 0, 0)
+
+    it "should get a list" do
+      VCR.use_cassette "tmdb" do
+        changes = Tmdb::Changes.movie
+
+        changes.total_items.should eq(1)
+      end
+    end
+
+    it "should iterate over all list" do
+      VCR.use_cassette "tmdb" do
+        changes = Tmdb::Changes.movie
+
+        changes.each do |change|
+          change.should be_a(Tmdb::Changes::Change)
+        end
+      end
+    end
+
+    it "should filter by start date" do
+      VCR.use_cassette "tmdb" do
+        changes = Tmdb::Changes.movie(start_date: start_date)
+        n = 0
+
+        changes.each do |change|
+          n += 1
+          change.should be_a(Tmdb::Changes::Change)
+        end
+
+        n.should eq(10337)
+      end
+    end
+
+    it "should filter by end date" do
+      VCR.use_cassette "tmdb" do
+        changes = Tmdb::Changes.movie(end_date: end_date)
+        n = 0
+
+        changes.each do |change|
+          n += 1
+          change.should be_a(Tmdb::Changes::Change)
+        end
+
+        n.should eq(9793)
+      end
+    end
+
+    it "should filter both by start and end date" do
+      VCR.use_cassette "tmdb" do
+        changes = Tmdb::Changes.movie(start_date: start_date, end_date: end_date)
+        n = 0
+
+        changes.each do |change|
+          n += 1
+          change.should be_a(Tmdb::Changes::Change)
+        end
+
+        n.should eq(4495)
+      end
+    end
+  end
+
+  context "#tv_show" do
+    start_date = Time.utc(2018, 5, 20, 10, 0, 0)
+    end_date = Time.utc(2018, 5, 25, 10, 0, 0)
+
+    it "should get a list" do
+      VCR.use_cassette "tmdb" do
+        changes = Tmdb::Changes.tv_show
+
+        changes.total_items.should eq(1)
+      end
+    end
+
+    it "should iterate over all list" do
+      VCR.use_cassette "tmdb" do
+        changes = Tmdb::Changes.tv_show
+
+        changes.each do |change|
+          change.should be_a(Tmdb::Changes::Change)
+        end
+      end
+    end
+
+    it "should filter by start date" do
+      VCR.use_cassette "tmdb" do
+        changes = Tmdb::Changes.tv_show(start_date: start_date)
+        n = 0
+
+        changes.each do |change|
+          n += 1
+          change.should be_a(Tmdb::Changes::Change)
+        end
+
+        n.should eq(2519)
+      end
+    end
+
+    it "should filter by end date" do
+      VCR.use_cassette "tmdb" do
+        changes = Tmdb::Changes.tv_show(end_date: end_date)
+        n = 0
+
+        changes.each do |change|
+          n += 1
+          change.should be_a(Tmdb::Changes::Change)
+        end
+
+        n.should eq(2676)
+      end
+    end
+
+    it "should filter both by start and end date" do
+      VCR.use_cassette "tmdb" do
+        changes = Tmdb::Changes.tv_show(start_date: start_date, end_date: end_date)
+        n = 0
+
+        changes.each do |change|
+          n += 1
+          change.should be_a(Tmdb::Changes::Change)
+        end
+
+        n.should eq(1103)
+      end
+    end
+  end
+
+  context "#person" do
+    start_date = Time.utc(2018, 5, 20, 10, 0, 0)
+    end_date = Time.utc(2018, 5, 25, 10, 0, 0)
+
+    it "should get a list" do
+      VCR.use_cassette "tmdb" do
+        changes = Tmdb::Changes.person
+
+        changes.total_items.should eq(1)
+      end
+    end
+
+    it "should iterate over all list" do
+      VCR.use_cassette "tmdb" do
+        changes = Tmdb::Changes.person
+
+        changes.each do |change|
+          change.should be_a(Tmdb::Changes::Change)
+        end
+      end
+    end
+
+    it "should filter by start date" do
+      VCR.use_cassette "tmdb" do
+        changes = Tmdb::Changes.person(start_date: start_date)
+        n = 0
+
+        changes.each do |change|
+          n += 1
+          change.should be_a(Tmdb::Changes::Change)
+        end
+
+        n.should eq(15233)
+      end
+    end
+
+    it "should filter by end date" do
+      VCR.use_cassette "tmdb" do
+        changes = Tmdb::Changes.person(end_date: end_date)
+        n = 0
+
+        changes.each do |change|
+          n += 1
+          change.should be_a(Tmdb::Changes::Change)
+        end
+
+        n.should eq(14237)
+      end
+    end
+
+    it "should filter both by start and end date" do
+      VCR.use_cassette "tmdb" do
+        changes = Tmdb::Changes.person(start_date: start_date, end_date: end_date)
+        n = 0
+
+        changes.each do |change|
+          n += 1
+          change.should be_a(Tmdb::Changes::Change)
+        end
+
+        n.should eq(6018)
+      end
+    end
+  end
+end
---
+++ src/changes.cr
@@ -0,0 +1,53 @@
+class Tmdb::Changes
+  class Change
+    getter id : Int64
+    getter? adult : Bool?
+
+    def initialize(data : JSON::Any)
+      @id = data["id"].as_i64
+      @adult = data["adult"].as_bool?
+    end
+  end
+
+  # Get a list of all of the movie ids that have been changed in the past 24
+  # hours.
+  #
+  # You can query it for up to 14 days worth of changed IDs at a time with the
+  # `start_date` and `end_date` query parameters.
+  def self.movie(start_date : Time? = nil, end_date : Time? = nil) : LazyIterator(Change)
+    filters = FilterFactory::Filter.new
+    filters[:start_date] = start_date.to_s("%Y-%m-%d") unless start_date.nil?
+    filters[:end_date] = end_date.to_s("%Y-%m-%d") unless end_date.nil?
+
+    res = Resource.new("/movie/changes", filters)
+    LazyIterator(Change).new(res)
+  end
+
+  # Get a list of all of the TV show ids that have been changed in the past 24
+  # hours.
+  #
+  # You can query it for up to 14 days worth of changed IDs at a time with the
+  # `start_date` and `end_date` query parameters.
+  def self.tv_show(start_date : Time? = nil, end_date : Time? = nil) : LazyIterator(Change)
+    filters = FilterFactory::Filter.new
+    filters[:start_date] = start_date.to_s("%Y-%m-%d") unless start_date.nil?
+    filters[:end_date] = end_date.to_s("%Y-%m-%d") unless end_date.nil?
+
+    res = Resource.new("/tv/changes", filters)
+    LazyIterator(Change).new(res)
+  end
+
+  # Get a list of all of the people ids that have been changed in the past 24
+  # hours.
+  #
+  # You can query it for up to 14 days worth of changed IDs at a time with the
+  # `start_date` and `end_date` query parameters.
+  def self.person(start_date : Time? = nil, end_date : Time? = nil) : LazyIterator(Change)
+    filters = FilterFactory::Filter.new
+    filters[:start_date] = start_date.to_s("%Y-%m-%d") unless start_date.nil?
+    filters[:end_date] = end_date.to_s("%Y-%m-%d") unless end_date.nil?
+
+    res = Resource.new("/person/changes", filters)
+    LazyIterator(Change).new(res)
+  end
+end
