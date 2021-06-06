7e1e58728c43c16bc4fbd4d091dcf44336bc106c
Moisès Macià <mmacia@gmail.com>
2021-06-06 23:05:34 +0200

    minor fixes

--- src/movie/cast.cr
+++ src/movie/cast.cr
@@ -10,7 +10,15 @@ class Tmdb::Movie
       super(data)
 
       @cast_id = data["cast_id"].as_i
-      @character = data["character"].as_s?
+
+      chr = data["character"].as_s?
+      character = if chr.nil?
+                    nil
+                  else
+                    chr.not_nil!.empty? ? nil : chr.not_nil!
+                  end
+
+      @character = character
       @order = data["order"].as_i
     end
   end
--- src/release.cr
+++ src/release.cr
@@ -13,13 +13,29 @@ class Tmdb::Release
   getter iso_639_1 : String?
   getter release_date : Time
   getter type : Type
-  getter note : String
+  getter note : String?
 
   def initialize(data : JSON::Any)
     @certification = data["certification"].as_s
-    @iso_639_1 = data["iso_639_1"].as_s?
+
+    lang = data["iso_639_1"].as_s?
+    iso_639_1 = if lang
+                  lang.not_nil!.empty? ? nil : lang.not_nil!
+                else
+                  nil
+                end
+
+    @iso_639_1 = iso_639_1
     @release_date = Time.parse(data["release_date"].as_s, "%Y-%m-%d", Time::Location::UTC)
     @type = Type.from_value(data["type"].as_i)
-    @note = data["note"].as_s
+
+    n = data["note"]? ? data["note"].as_s : nil
+    note = if n
+             n.not_nil!.empty? ? nil : n.not_nil!
+           else
+             nil
+           end
+
+    @note = note
   end
 end
