f16fb3cf47d21b4ecd8e987ba47825a99fb3c20f
Moisès Macià <mmacia@gmail.com>
2021-06-21 22:55:08 +0200

    crew/cast to person detail

--- src/tv/cast_base.cr
+++ src/tv/cast_base.cr
@@ -17,11 +17,15 @@ abstract class Tmdb::Tv::CastBase
     @adult = data["adult"].as_bool
     @gender = data["gender"]? ? Tmdb::Person::Gender.from_value(data["gender"].as_i) : nil
     @id = data["id"].as_i64
-    @known_for_department = data["known_for_department"].as_s
+    @known_for_department = data["known_for_department"].as_s?
     @name = data["name"].as_s
     @original_name = data["original_name"].as_s
     @popularity = data["popularity"].as_f
     @profile_path = data["profile_path"].as_s?
     @order = data["order"].as_i
+  end
+
+  def person_detail : Person
+    Person.detail id
   end
 end
--- src/tv/crew_base.cr
+++ src/tv/crew_base.cr
@@ -17,11 +17,15 @@ abstract class Tmdb::Tv::CrewBase
     @adult = data["adult"].as_bool
     @gender = data["gender"]? ? Tmdb::Person::Gender.from_value(data["gender"].as_i) : nil
     @id = data["id"].as_i64
-    @known_for_department = data["known_for_department"].as_s
+    @known_for_department = data["known_for_department"].as_s?
     @name = data["name"].as_s
     @original_name = data["original_name"].as_s
     @popularity = data["popularity"].as_f
     @profile_path = data["profile_path"].as_s?
     @department = data["department"].as_s
+  end
+
+  def person_detail : Person
+    Person.detail id
   end
 end
